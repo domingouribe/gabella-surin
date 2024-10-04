c6a0067d6334ce5e84c022ce515706a869b9fed5
domingouribe <uribe.domingo@gmail.com>
2024-10-04 16:19:41 -0300

    responsive styles done

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -1,18 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const articles = document.querySelectorAll(".Article");
+// //FIle name: accordion.js
+// document.addEventListener("DOMContentLoaded", () => {
+//   const articles = document.querySelectorAll(".Article");
 
-  articles.forEach((article) => {
-    const header = article.querySelector(".Article-header");
-    const content = article.querySelector(".Article-content");
+//   articles.forEach((article) => {
+//     const header = article.querySelector(".Article-header");
+//     const content = article.querySelector(".Article-content");
 
-    header.addEventListener("click", () => {
-      article.classList.toggle("active");
+//     header.addEventListener("click", () => {
+//       article.classList.toggle("active");
 
-      if (article.classList.contains("active")) {
-        content.style.maxHeight = content.scrollHeight + "px";
-      } else {
-        content.style.maxHeight = 0;
-      }
-    });
-  });
-});
+//       if (article.classList.contains("active")) {
+//         content.style.maxHeight = content.scrollHeight + "px";
+//       } else {
+//         content.style.maxHeight = 0;
+//       }
+//     });
+//   });
+// });
